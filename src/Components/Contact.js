c7f55de9423ce47e6c9639b5ad5ed73e64218cbf
Brenda Gonzalez Ortega <brendago@MacBook-Pro-de-Brenda.local>
2021-09-05 22:29:39 -0500

    map fixes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -49,10 +49,10 @@ class Contact extends Component {
                     </div>
                 </section>
                 <section id="contact" className="contacto">
-                    <div className="contact_descript">
+                    <div className="contact_map">
                         <iframe title="company map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2486.678357055867!2d5.524512815767693!3d51.44570057962508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c6d8984df40151%3A0x819e5775916b5124!2sUrkhovenseweg%2017%2C%205641%20KA%20Eindhoven%2C%20Netherlands!5e0!3m2!1sen!2smx!4v1628481560922!5m2!1sen!2smx" style={{ border: "0" }} allowfullscreen="" loading="lazy" />
                     </div>
-                    <div>
+                    <div className="contact_descript">
                         <h2>Contact us</h2>
                         <p>We will get in touch with you as soon as possible.</p>
                         <form onSubmit={this.sendEmailWithService} className="invite-form">
